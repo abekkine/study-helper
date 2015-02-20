07a56cf3f9973a861dd82ec15ee95d618be1a73e
A.Bekkine <abekkine@gmail.com>
2015-02-20 14:36:15 +0200

    Paper Test updates
    - Html update to include jquery.
    - Js updated to run document "onLoad".

--- testing/paper/test.js
+++ testing/paper/test.js
@@ -1,27 +1,30 @@
-var createGameArea = function(width, height) {
-    var margin = 10;
-    var area = new Rectangle(margin, margin, width, height);
-    var path = new Path.Rectangle(area);
+$(function() {
 
-    path.fillColor = '#ccf';
-};
+    var createGameArea = function(width, height) {
+        var margin = 10;
+        var area = new Rectangle(margin, margin, width, height);
+        var path = new Path.Rectangle(area);
 
-var createCell = function(x, y) {
-    var baseMargin = 10;
-    var margin = 5;
-    var size = 45;
-    var xPos = x * (size + margin) + margin;
-    var yPos = y * (size + margin) + margin;
-    var cell = new Rectangle(baseMargin + xPos, baseMargin + yPos, size, size);
-    var path = new Path.Rectangle(cell);
+        path.fillColor = '#ccf';
+    };
 
-    path.fillColor = '#4c4';
-};
+    var createCell = function(x, y) {
+        var baseMargin = 10;
+        var margin = 5;
+        var size = 45;
+        var xPos = x * (size + margin) + margin;
+        var yPos = y * (size + margin) + margin;
+        var cell = new Rectangle(baseMargin + xPos, baseMargin + yPos, size, size);
+        var path = new Path.Rectangle(cell);
+
+        path.fillColor = '#4c4';
+    };
 
 
-createGameArea(505, 505);
-for(var i=0; i<10; i++) {
-    xPos = parseInt(Math.random() * 10);
-    yPos = parseInt(Math.random() * 10);
-    createCell(xPos, yPos);
-}
+    createGameArea(505, 505);
+    for(var i=0; i<10; i++) {
+        xPos = parseInt(Math.random() * 10);
+        yPos = parseInt(Math.random() * 10);
+        createCell(xPos, yPos);
+    }
+});
