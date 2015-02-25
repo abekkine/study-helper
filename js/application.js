8d4cf6f873ad222d4262cb40d7b25a7ed3e05ac0
A.Bekkine <abekkine@gmail.com>
2015-02-25 11:57:49 +0200

    Testing interfaces
    - Test buttons and functions removed.
    - TODO file removed, since github issues are used instead.

--- js/application.js
+++ js/application.js
@@ -5,21 +5,6 @@ $(function() {
         INPUT.AddOneCharacter(event.which);
     });
 
-    $('#act_true').click(function() {
-        console.log('true click');
-        GAME.PlayerAction(true);
-    });
-
-    $('#act_false').click(function() {
-        console.log('false click');
-        GAME.PlayerAction(false);
-    });
-
-    $('#act_next').click(function() {
-        console.log('next click');
-        QUESTION.NewQuestion();
-    });
-
     GAME.Setup('game', 515, 515);
 
     QUESTION.NewQuestion();
