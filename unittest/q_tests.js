cc4e590138df224cdf686fef786ee82517907a5f
A.Bekkine <abekkine@gmail.com>
2015-04-01 22:21:02 +0300

    Unit test for sub_one added (See #28)

--- unittest/q_tests.js
+++ unittest/q_tests.js
@@ -1,5 +1,6 @@
 var util = require('../js/util').util;
 var add_one = require('../js/q/add_one').add_one;
+var sub_one = require('../js/q/sub_one').sub_one;
 
 exports['add_one'] = function(test) {
 
@@ -16,3 +17,17 @@ exports['add_one'] = function(test) {
     test.done();
 };
 
+exports['sub_one'] = function(test) {
+
+    var q = sub_one(util);
+
+    test.expect(6);
+
+    test.equal(q.operator, '-');
+    test.equal(q.operand_a - q.operand_b, q.result);
+    test.equal(q.valid_length, 1);
+    test.equal(q.operand_a >= 0 && q.operand_a <= 9, true);
+    test.equal(q.operand_b >= 0 && q.operand_b <= 9, true);
+    test.equal(q.result >= 0 && q.result <= 9, true);
+    test.done()    
+};
