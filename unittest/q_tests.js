4280935022894d2d9f1c731a8d648c572f69dd8e
A.Bekkine <abekkine@gmail.com>
2015-04-04 08:48:02 +0300

    Unit tests for question modules completed (See #28).

--- unittest/q_tests.js
+++ unittest/q_tests.js
@@ -41,8 +41,26 @@ exports['add_two'] = function(test) {
 
     var q = add_two(util);
 
+    test.expect(7);
+
+    // Our operator should be addition.
     test.equal(q.operator, '+');
-    test.ok(false, 'Fail since incomplete');
+    // Maximum result length is 2 digits.
+    test.equal(q.valid_length, 2);
+    // Operation should be correct.
+    test.equal(q.operand_a + q.operand_b, q.result);
+    // Operands and result should be two digits with proper ranges.
+    test.equal(q.operand_a >= 10 && q.operand_a <= 89, true);
+    test.equal(q.operand_b >= 10 && q.operand_b <= 89, true);
+    test.equal(q.result >= 20 && q.result <= 99, true);
+
+    var dig_b = q.operand_a % 10,
+        dig_d = q.operand_b % 10,
+        sum = dig_b + dig_d;
+
+    // And this should be a operation 'without carry'.
+    test.equal(sum >= 0 && sum <= 9, true);
+
     test.done();
 };
 
@@ -50,8 +68,21 @@ exports['add_two_carry'] = function(test) {
 
     var q = add_two_carry(util);
 
+    test.expect(7);
+
     test.equal(q.operator, '+');
-    test.ok(false, 'Fail since incomplete');
+    test.equal(q.valid_length, 2);
+    test.equal(q.operand_a + q.operand_b, q.result);
+    test.equal(q.operand_a >= 10 && q.operand_a <= 89, true);
+    test.equal(q.operand_b >= 10 && q.operand_b <= 89, true);
+    test.equal(q.result >= 20 && q.result <= 99, true);
+
+    var dig_b = q.operand_a % 10,
+        dig_d = q.operand_b % 10,
+        sum = dig_b + dig_d;
+
+    test.equal(sum >= 10 && sum <= 18, true);
+
     test.done();
 };
 
@@ -74,8 +105,20 @@ exports['sub_two'] = function(test) {
 
     var q = sub_two(util);
 
+    test.expect(7);
+
     test.equal(q.operator, '-');
-    test.ok(false, 'Fail since incomplete');
+    test.equal(q.valid_length, 2);
+    test.equal(q.operand_a - q.operand_b, q.result);
+    test.equal(q.operand_b >= 10 && q.operand_b <= 89, true);
+    test.equal(q.operand_a >= 20 && q.operand_a <= 99, true);
+    test.equal(q.result >= 10 && q.result <= 89, true);
+
+    var dig_b = q.operand_a % 10,
+        dig_d = q.operand_b % 10,
+        dif = dig_b - dig_d;
+   
+    test.equal(dif >= 0 && dif <= 9, true);
     test.done();
 };
 
@@ -84,7 +127,17 @@ exports['sub_two_borrow'] = function(test) {
     var q = sub_two_borrow(util);
 
     test.equal(q.operator, '-');
-    test.ok(false, 'Fail since incomplete');
+    test.equal(q.valid_length, 2);
+    test.equal(q.operand_a - q.operand_b, q.result);
+    test.equal(q.operand_b >= 10 && q.operand_b <= 89, true);
+    test.equal(q.operand_a >= 10 && q.operand_a <= 99, true);
+    test.equal(q.result >= 10 && q.result <= 89, true);
+
+    var dig_b = q.operand_a % 10,
+        dig_d = q.operand_b % 10,
+        dif = dig_b - dig_d;
+
+    test.equal(dif <= 0 && dif >= -9, true);
     test.done();
 };
 
